e5e2ba7e7f657ccea6eed0159d15bd341949b7c8
FranckyPoop <afrifranck2003@gmail.com>
2022-02-02 22:55:48 +0000

    Ajour du fichier main

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import os
 from fonctions import fonctionnality
 
-folder_current = os.getcwd()
-folder_data = os.path.join(folder_current, "data")
-
-fonctionnality.connection()
+fonctionnality.appoitment()
