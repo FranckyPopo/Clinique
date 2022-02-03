7c5c083bdcd9c47085f21536fd2465a90951680f
FranckyPopo <afrifranck2003@gmail.com>
2022-02-03 16:10:33 +0000

    Création de la fenêtre princiapale utilisateur

--- doctor/main.py
+++ doctor/main.py
@@ -1,2 +1,17 @@
+import tkinter
 from fonctions_doctor import fonctionnality_doctor
 
+window = tkinter.Tk()
+window.geometry("1080x720")
+window.title("Clinique Popo")
+window.minsize(1080, 720)
+
+frame_1 = tkinter.Frame(window, bg="#FFFFFF", width=540)
+frame_1.pack(fill="y", side="left")
+
+frame_2 = tkinter.Frame(window, bg="#15AED6", width=540)
+frame_2.pack(fill="y", side="right")
+
+image = None
+
+window.mainloop()
