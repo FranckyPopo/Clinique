d5afeb062cff89315c9516dd6747d104010e4a1a
FranckyPopo <afrifranck2003@gmail.com>
2022-02-03 14:42:47 +0000

    Fin de la création de la fonctionnalité modify notebook

--- doctor/fonctions_doctor/fonctionnality_doctor.py
+++ doctor/fonctions_doctor/fonctionnality_doctor.py
@@ -5,24 +5,33 @@ folder_current = os.getcwd().replace("/doctor", "")
 folder_data = os.path.join(folder_current, "data_programme")
 
 list_patient = get_data(folder_data, "list_patients")
+list_notebook = get_data(folder_data, "list_notebook")  
 
 
-def fill_canet():
+def fill_notebook():
     id_patient = input("Veuillez entrer l'identifiant du patient: ")
     reason = input("Veuillez remplir le canet: ")  
     
-    list_canet = get_data(folder_data, "list_canet")  
     canet = {"id": id_patient, "reason": reason}
-    list_canet.append(canet)
+    list_notebook.append(canet)
     
     for item in list_patient:
         if item["id"] == id_patient:
-            recording_data(list_canet, folder_current, "data_programme", "list_canet")
+            recording_data(list_notebook, folder_current, "data_programme", "list_notebook")
             break
     else:
-        print("L'identifiant du partient est incorrecte")
+        print("L'identifiant n'es pas attribué a un patient")
             
 
+def modify_notebook():
+    id_patient = input("Veuillez entrer l'identifiant du patient: ")
+    new_reason = input("Veuillez remplir le canet: ")  
     
-    
+    for item in list_notebook:
+        if item["id"] == id_patient:
+            item["reason"] = new_reason
+            recording_data(list_notebook, folder_current, "data_programme", "list_notebook")
+            break
+    else:
+        print("L'identifiant n'es pas attribué a un patient")
     
