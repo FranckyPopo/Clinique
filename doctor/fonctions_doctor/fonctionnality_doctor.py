071cb77eed34172c3a1d9f8eadae25406eff0bf8
FranckyPopo <afrifranck2003@gmail.com>
2022-02-03 14:08:14 +0000

    Fin de la création de la fonctionnalité rempli carnet

--- doctor/fonctions_doctor/fonctionnality_doctor.py
+++ doctor/fonctions_doctor/fonctionnality_doctor.py
@@ -8,19 +8,19 @@ list_patient = get_data(folder_data, "list_patients")
 
 
 def fill_canet():
-    id_patient = input("Veuillez entrer l'identifiant du patient")
-    reason = input("Veuillez remplir le canet")  
+    id_patient = input("Veuillez entrer l'identifiant du patient: ")
+    reason = input("Veuillez remplir le canet: ")  
     
     list_canet = get_data(folder_data, "list_canet")  
     canet = {"id": id_patient, "reason": reason}
     list_canet.append(canet)
     
     for item in list_patient:
-        if item["last_name"] == id_patient:
+        if item["id"] == id_patient:
             recording_data(list_canet, folder_current, "data_programme", "list_canet")
             break
     else:
-        print("Identifiant du partient est incorrecte")
+        print("L'identifiant du partient est incorrecte")
             
 
     
