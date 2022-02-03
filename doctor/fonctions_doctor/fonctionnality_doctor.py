70f768cd7d044e7730f89ee15e6965d2f07b1332
FranckyPopo <afrifranck2003@gmail.com>
2022-02-03 12:58:25 +0000

    Débu de la création de la fonctionnalité remplir canet

---
+++ doctor/fonctions_doctor/fonctionnality_doctor.py
@@ -0,0 +1,28 @@
+import os
+from patient.fonctions.data import get_data, recording_data
+
+folder_current = os.getcwd().replace("/doctor", "")
+folder_data = os.path.join(folder_current, "data_programme")
+
+list_patient = get_data(folder_data, "list_patients")
+
+
+def fill_canet():
+    id_patient = input("Veuillez entrer l'identifiant du patient")
+    reason = input("Veuillez remplir le canet")  
+    
+    list_canet = get_data(folder_data, "list_canet")  
+    canet = {"id": id_patient, "reason": reason}
+    list_canet.append(canet)
+    
+    for item in list_patient:
+        if item["last_name"] == id_patient:
+            recording_data(list_canet, folder_current, "data_programme", "list_canet")
+            break
+    else:
+        print("Identifiant du partient est incorrecte")
+            
+
+    
+    
+    
