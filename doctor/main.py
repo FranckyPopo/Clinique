730746a2d93c0319d12b86a9bcfcbe755aadacbe
FranckyPoop <afrifranck2003@gmail.com>
2022-02-06 17:20:55 +0000

    Ajout de ka finctionalité voir rendez-vous

--- doctor/main.py
+++ doctor/main.py
@@ -17,7 +17,7 @@ list_reason = get_data(folder_data, "list_reason")
 list_doctors = [
     {
         "name_doctor": "Afri Franck",
-        "specialist": "Géneraliste",
+        "specialist": "Genéraliste",
         "registation_number": "AAAA1",
         "password": "AAAA1"
     },
@@ -29,7 +29,7 @@ list_doctors = [
     },
     {
         "name_doctor": "Francky Popo",
-        "specialist": "Dermatogue",
+        "specialist": "Dermatologue",
         "registation_number": "AAAA3",
         "password": "AAAA3"
     },
@@ -46,7 +46,7 @@ list_doctors = [
         "password": "AAAA5"
     }
 ]
-
+_ID = {}
 
 def connection():
     def check_connection():
@@ -59,7 +59,11 @@ def connection():
                     label_error["fg"] = "#15AED6"
                     enter_password.delete(0, "end")
                     enter_registation_number.delete(0, "end")
-                    #window_user()
+                    
+                    for key, value in item.items():
+                        _ID[key] = value
+                    
+                    window_doctor()
                     break
             else:
                 label_error["fg"] = "#FA0000"    
@@ -91,6 +95,58 @@ def connection():
     bnt_connect = tkinter.Button(frame_container_connection, text="Se connecter", width=10, height=2, bg="#0e7993", command=check_connection, font=("Rubik", 13))
     bnt_connect.grid(row=6, column=0, pady=5, sticky="w")
  
+ 
+def window_doctor():
+    frame_container_connection.place_forget()
+    frame_container_see_appoitment.place_forget()
+    frame_container_see_notebook.place_forget()
+    frame_container_img.pack_forget()
+
+    frame_menu = tkinter.Frame(frame_1, bg="white")
+    frame_menu.place(x=0, y=110)
+    
+    bnt_see_appoitment = tkinter.Button(frame_menu, text="Voir les rendez-vous", bg="#eaeaea", width=76, height=3, command=see_appoitment, relief="raised")
+    bnt_see_appoitment.grid(row=0, column=0, pady=50)
+    
+    bnt_fill_notebook = tkinter.Button(frame_menu, text="Remplir un canet de santé", bg="#eaeaea", width=76, height=3, relief="raised")
+    bnt_fill_notebook.grid(row=1, column=0, pady=50)
+    
+    bnt_modify_notebook = tkinter.Button(frame_menu, text="Modifier un canet de sante", bg="#eaeaea", width=76, height=3, relief="raised")
+    bnt_modify_notebook.grid(row=2, column=0, pady=50)
+
+
+def see_appoitment():
+    def delete_appointment(id_delete: str, *label_delete):
+        for widget in label_delete:
+            widget.grid_forget()
+        
+        for item in list_appointment:
+            if item["id"] == id_delete:
+                list_appointment.remove(item)
+                recording_data(list_appointment, folder_clinique, "data_programme", "list_appoitment")
+                break
+
+    frame_container_see_notebook.place_forget()
+    frame_container_see_appoitment.place(x=80, y=150)
+    
+    title_date_appoitment = tkinter.Label(frame_container_see_appoitment, bg="#15AED6", text="Nom", font=("Arial", 18, "bold"))
+    title_date_appoitment.grid(row=0, column=0, sticky="w")
+    
+    label_doctor = tkinter.Label(frame_container_see_appoitment, bg="#15AED6", text="Prénom", font=("Arial", 18, "bold"))
+    label_doctor.grid(row=0, column=1, padx=75)
+    
+    i = 1
+    for appoitment in list_appointment:
+        if appoitment["choice_specialist"] == _ID["specialist"]:       
+            label_last_name = tkinter.Label(frame_container_see_appoitment, text=appoitment["last_name"], bg="#15AED6", fg="#1C1C1C", font=("Rubik", 13))
+            label_last_name.grid(row=i, column=0, sticky="w", padx=75, pady=10)
+             
+            label_firs_name = tkinter.Label(frame_container_see_appoitment, text=appoitment["firs_name"], bg="#15AED6", fg="#1C1C1C", font=("Rubik", 13))
+            label_firs_name.grid(row=i, column=1, sticky="w", padx=75, pady=10)
+            
+            bnt_cancel = tkinter.Button(frame_container_see_appoitment, text="Accepter", bg="#0e7993", fg="#1C1C1C", font=("Rubik", 13 , "bold"), relief="flat")
+            bnt_cancel.grid(row=i, column=2)  
+            i += 1 
 
 
 window = tkinter.Tk()
@@ -109,6 +165,9 @@ frame_main = tkinter.Frame(frame_2, bg="#15AED6")
 frame_main.place(x=80, y=200)
 
 frame_container_connection = tkinter.Frame(frame_2, bg="#15AED6")
+frame_container_see_appoitment = tkinter.Frame(frame_2, bg="#15AED6")
+frame_container_see_notebook = tkinter.Frame(frame_2, bg="#15AED6")
+
 
 frame_container_img = tkinter.Frame(frame_1, bg="white", width=540)
 frame_container_img.pack(side="left", fill="y")
