1143dc8141044808c2f011f5ad0ae58640c8f20f
FranckyPoop <afrifranck2003@gmail.com>
2022-02-07 11:20:34 +0000

    fin de l'algorithme qui va nous permetre de remplir les carnets des patients

--- doctor/main.py
+++ doctor/main.py
@@ -13,6 +13,7 @@ folder_data = os.path.join(folder_clinique, "data_programme")
 list_patients = get_data(folder_data, "list_patients")
 list_appointment = get_data(folder_data, "list_appoitment")
 list_reason = get_data(folder_data, "list_reason")
+list_notebook = get_data(folder_data, "list_notebook")
 
 list_doctors = [
     {
@@ -60,6 +61,7 @@ def connection():
                     enter_password.delete(0, "end")
                     enter_registation_number.delete(0, "end")
                     
+                    # Récupération des information du docteur
                     for key, value in item.items():
                         _ID[key] = value
                     
@@ -92,8 +94,8 @@ def connection():
     label_error = tkinter.Label(frame_container_connection, text="Impossible de vous connecter", fg="#15AED6", bg="#15AED6", font=("Arial", 12, "bold"))
     label_error.grid(row=5, column=0, sticky="w", pady=7)
     
-    bnt_connect = tkinter.Button(frame_container_connection, text="Se connecter", width=10, height=2, bg="#0e7993", command=check_connection, font=("Rubik", 13))
-    bnt_connect.grid(row=6, column=0, pady=5, sticky="w")
+    bnt_connect = tkinter.Button(frame_container_connection, text="Se connecter", width=10, height=2, bg="#0e7993", command=check_connection, font=("Rubik", 13), relief="flat")
+    bnt_connect.grid(row=6, column=0, pady=5, sticky="w", ipadx=3, ipady=2)
  
  
 def window_doctor():
@@ -108,7 +110,7 @@ def window_doctor():
     bnt_see_appoitment = tkinter.Button(frame_menu, text="Voir les rendez-vous", bg="#eaeaea", width=76, height=3, command=see_appoitment, relief="raised")
     bnt_see_appoitment.grid(row=0, column=0, pady=50)
     
-    bnt_fill_notebook = tkinter.Button(frame_menu, text="Remplir un canet de santé", bg="#eaeaea", width=76, height=3, relief="raised")
+    bnt_fill_notebook = tkinter.Button(frame_menu, text="Remplir un canet de santé", bg="#eaeaea", width=76, height=3, relief="raised", command=fill_notebook)
     bnt_fill_notebook.grid(row=1, column=0, pady=50)
     
     bnt_modify_notebook = tkinter.Button(frame_menu, text="Modifier un canet de sante", bg="#eaeaea", width=76, height=3, relief="raised")
@@ -127,26 +129,67 @@ def see_appoitment():
                 break
 
     frame_container_see_notebook.place_forget()
+    frame_container_fill_notebook.place_forget()
     frame_container_see_appoitment.place(x=80, y=150)
     
-    title_date_appoitment = tkinter.Label(frame_container_see_appoitment, bg="#15AED6", text="Nom", font=("Arial", 18, "bold"))
+    title_date_appoitment = tkinter.Label(frame_container_see_appoitment, bg="#15AED6", text="Nom", font=("Rubik", 18, "bold"))
     title_date_appoitment.grid(row=0, column=0, sticky="w")
     
-    label_doctor = tkinter.Label(frame_container_see_appoitment, bg="#15AED6", text="Prénom", font=("Arial", 18, "bold"))
+    label_doctor = tkinter.Label(frame_container_see_appoitment, bg="#15AED6", text="Prénom", font=("Rubik", 18, "bold"))
     label_doctor.grid(row=0, column=1, padx=75)
     
     i = 1
     for appoitment in list_appointment:
         if appoitment["choice_specialist"] == _ID["specialist"]:       
             label_last_name = tkinter.Label(frame_container_see_appoitment, text=appoitment["last_name"], bg="#15AED6", fg="#1C1C1C", font=("Rubik", 13))
-            label_last_name.grid(row=i, column=0, sticky="w", padx=75, pady=10)
+            label_last_name.grid(row=i, column=0, sticky="w", pady=10)
              
             label_firs_name = tkinter.Label(frame_container_see_appoitment, text=appoitment["firs_name"], bg="#15AED6", fg="#1C1C1C", font=("Rubik", 13))
             label_firs_name.grid(row=i, column=1, sticky="w", padx=75, pady=10)
             
             bnt_cancel = tkinter.Button(frame_container_see_appoitment, text="Accepter", bg="#0e7993", fg="#1C1C1C", font=("Rubik", 13 , "bold"), relief="flat")
-            bnt_cancel.grid(row=i, column=2)  
+            bnt_cancel.grid(row=i, column=2, sticky="w")  
             i += 1 
+
+
+def fill_notebook():
+    def check_fill_notebook():
+        id_patient = enter_id.get()
+        reason = enter_reason.get()
+        
+        for item in list_appointment:
+            for patient in list_patients: 
+                if item["id"] == id_patient and patient["phone"] == item["phone"] and patient["last_name"] == item["last_name"]:
+                    notebook = {"id": id_patient, "reason": reason, "date": None}
+                    list_notebook.append(notebook)
+                    recording_data(list_notebook, folder_clinique, "data_programme", "list_notebook")
+                    break
+        else:
+            label_error["fg"] = "#FA0000"       
+    
+    frame_container_see_appoitment.place_forget()
+    frame_container_see_notebook.place_forget()
+    frame_container_fill_notebook.place(x=100, y=100)
+    
+    label_id = tkinter.Label(frame_container_fill_notebook, bg="#15AED6", text="Identifiant du patient", font=("Rubik", 18))
+    label_id.grid(row=0, column=0, pady=5, sticky="w")
+    
+    enter_id = tkinter.Entry(frame_container_fill_notebook, width=30)
+    enter_id.grid(row=1, column=0, pady=5, sticky="w")
+    
+    
+    label_reason= tkinter.Label(frame_container_fill_notebook, bg="#15AED6", text="Motif", font=("Rubik", 18))
+    label_reason.grid(row=2, column=0, pady=5, sticky="w")
+    
+    enter_reason = tkinter.Entry(frame_container_fill_notebook, width=30)
+    enter_reason.grid(row=3, column=0, pady=5, sticky="w")
+    
+    label_error = tkinter.Label(frame_container_fill_notebook, bg="#15AED6", fg="#15AED6", text="Veuillez remplir tous les champs", font=("Rubik", 13, "bold"))
+    label_error.grid(row=4, column=0, pady=5, sticky="w")
+    
+    bnt_fill_notebook = tkinter.Button(frame_container_fill_notebook, text="Remplir le canet", bg="#0e7993", font=("Rubik", 13), relief="flat", command=check_fill_notebook)
+    bnt_fill_notebook.grid(row=5, column=0, pady=5, ipadx=3, ipady=2)
+    
 
 
 window = tkinter.Tk()
@@ -167,7 +210,7 @@ frame_main.place(x=80, y=200)
 frame_container_connection = tkinter.Frame(frame_2, bg="#15AED6")
 frame_container_see_appoitment = tkinter.Frame(frame_2, bg="#15AED6")
 frame_container_see_notebook = tkinter.Frame(frame_2, bg="#15AED6")
-
+frame_container_fill_notebook = tkinter.Frame(frame_2, bg="#15AED6")
 
 frame_container_img = tkinter.Frame(frame_1, bg="white", width=540)
 frame_container_img.pack(side="left", fill="y")
@@ -185,8 +228,8 @@ image_label.place(x= 80, y=170)
 label_title_1 = tkinter.Label(frame_title, justify="left", text="Ensemble, prenons soin de \nvotre santé", font=("Rubik", 24), bg="#15AED6", fg="white")
 label_title_1.grid(row=1, column=0, sticky="w")
 
-bnt_connect = tkinter.Button(frame_main, text="Se connecter", bg="#0e7993", width=10, height=2, font=("Arial", 12), command=connection)
-bnt_connect.grid(row=4, column=0, sticky="w", pady=5)
+bnt_connect = tkinter.Button(frame_main, text="Se connecter", bg="#0e7993", width=10, height=2, font=("Rubik", 13), command=connection, relief="flat")
+bnt_connect.grid(row=4, column=0, sticky="w", pady=5, ipadx=3, ipady=2)
 
 
 window.mainloop()
