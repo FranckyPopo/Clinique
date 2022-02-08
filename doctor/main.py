4aa03041cd9d456fccc166d3c355c407a5e643e5
FranckyPoop <afrifranck2003@gmail.com>
2022-02-08 01:26:06 +0000

    Améliration de certain widget

--- doctor/main.py
+++ doctor/main.py
@@ -78,10 +78,10 @@ def connection():
     
     frame_container_connection.place(x=150, y=140)  
         
-    label_title = tkinter.Label(frame_container_connection, text="Connectez-vous", bg="#15AED6", font=("Arial", 24, "bold"))
+    label_title = tkinter.Label(frame_container_connection, text="Connectez-vous", bg="#15AED6", fg="#1C1C1C", font=("Rubik", 24, "bold"))
     label_title.grid(row=0, column=0, pady=15)
     
-    label_registation_number = tkinter.Label(frame_container_connection, text="Matricule", bg="#15AED6", font=("Rubik", 16), fg="#1C1C1C")
+    label_registation_number = tkinter.Label(frame_container_connection, text="Matricule:", bg="#15AED6", font=("Rubik", 16), fg="#1C1C1C")
     label_registation_number.grid(row=1, column=0, sticky="w")
     
     enter_registation_number = tkinter.Entry(frame_container_connection)
@@ -93,11 +93,11 @@ def connection():
     enter_password = tkinter.Entry(frame_container_connection)
     enter_password.grid(row=4, column=0, sticky="w", pady=5)
     
-    label_error = tkinter.Label(frame_container_connection, text="Impossible de vous connecter", fg="#15AED6", bg="#15AED6", font=("Arial", 12, "bold"))
+    label_error = tkinter.Label(frame_container_connection, text="Impossible de vous connecter", fg="#15AED6", bg="#15AED6", font=("Rubik   ", 12, "bold"))
     label_error.grid(row=5, column=0, sticky="w", pady=7)
     
-    bnt_connect = tkinter.Button(frame_container_connection, text="Se connecter", width=10, height=2, bg="#0e7993", command=check_connection, font=("Rubik", 13), relief="flat")
-    bnt_connect.grid(row=6, column=0, pady=5, sticky="w", ipadx=3, ipady=2)
+    bnt_connect = tkinter.Button(frame_container_connection, text="Se connecter", bg="#0e7993", command=check_connection, font=("Rubik", 13), relief="flat")
+    bnt_connect.grid(row=6, column=0, pady=5, sticky="w", ipadx=5, ipady=2)
  
  
 def window_doctor():
@@ -132,6 +132,8 @@ def see_appoitment():
 
     frame_container_see_notebook.place_forget()
     frame_container_fill_notebook.place_forget()
+    frame_container_modify_notebook.place_forget()
+    frame_container_test.place_forget()
     frame_container_see_appoitment.place(x=80, y=150)
     
     title_date_appoitment = tkinter.Label(frame_container_see_appoitment, bg="#15AED6", text="Nom", font=("Rubik", 18, "bold"))
@@ -187,6 +189,8 @@ def fill_notebook():
     
     frame_container_see_appoitment.place_forget()
     frame_container_see_notebook.place_forget()
+    frame_container_modify_notebook.place_forget()
+    frame_container_test.place_forget()
     frame_container_fill_notebook.place(x=160, y=140,)
     
     label_id = tkinter.Label(frame_container_fill_notebook, bg="#15AED6", text="Identifiant du rendez-vous", font=("Rubik", 18))
@@ -225,7 +229,7 @@ def modify_notebook():
         for item in list_notebook:
             if item["id"] == id_appointment:
                 frame_container_modify_notebook.place_forget()
-                frame_container_test.place(x=100, y=100)
+                frame_container_test.place(x=120, y=180)
                 
                 label_new_reason = tkinter.Label(frame_container_test, text="Motif:", bg="#15AED6", font=("Rubik", 18))
                 label_new_reason.grid(row=0, column=0, sticky="w", pady=5)
@@ -244,7 +248,8 @@ def modify_notebook():
 
     frame_container_fill_notebook.place_forget()
     frame_container_see_appoitment.place_forget()
-    frame_container_modify_notebook.place(x=100, y=100)
+    frame_container_test.place_forget()
+    frame_container_modify_notebook.place(x=150, y=200)
  
     label_id_appointment = tkinter.Label(frame_container_modify_notebook, text="Entrer l'id su rendez-vous", bg="#15AED6", fg="#1C1C1C", font=("Rubik", 18))
     label_id_appointment.grid(row=0, column=0, sticky="w", pady=3)
@@ -258,14 +263,11 @@ def modify_notebook():
     bnt_validate_id_appoitment = tkinter.Button(frame_container_modify_notebook, relief="flat", text="Valider", command=check_id, bg="#0e7993")
     bnt_validate_id_appoitment.grid(row=3, column=0, sticky="w", ipadx=3, ipady=2, pady=3)
     
-    
-    
 
 window = tkinter.Tk()
 window.geometry("1080x720")
 window.title("Clinique Popo")
 window.resizable(False, False)
-window["bg"] = "white"
 
 frame_1 = tkinter.Frame(window, bg="white", width=540)
 frame_1.pack(side="left", fill="y")
@@ -283,7 +285,6 @@ frame_container_fill_notebook = tkinter.Frame(frame_2, bg="#15AED6")
 frame_container_modify_notebook = tkinter.Frame(frame_2, bg="#15AED6")
 frame_container_test = tkinter.Frame(frame_2, bg="#15AED6")
 
-
 frame_container_img = tkinter.Frame(frame_1, bg="white", width=540)
 frame_container_img.pack(side="left", fill="y")
 
@@ -300,8 +301,8 @@ image_label.place(x= 80, y=170)
 label_title_1 = tkinter.Label(frame_title, justify="left", text="Ensemble, prenons soin de \nvotre santé", font=("Rubik", 24), bg="#15AED6", fg="white")
 label_title_1.grid(row=1, column=0, sticky="w")
 
-bnt_connect = tkinter.Button(frame_main, text="Se connecter", bg="#0e7993", width=10, height=2, font=("Rubik", 13), command=connection, relief="flat")
-bnt_connect.grid(row=4, column=0, sticky="w", pady=5, ipadx=3, ipady=2)
+bnt_connect = tkinter.Button(frame_main, text="Se connecter", bg="#0e7993", font=("Rubik", 13), command=connection, relief="flat")
+bnt_connect.grid(row=4, column=0, sticky="w", pady=5, ipadx=5, ipady=2)
 
 
 window.mainloop()
